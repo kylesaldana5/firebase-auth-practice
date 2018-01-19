146083a527184a29baeec50d7d9f3e0fa30f583f
kylesaldana5 <kylesaldana5@gmail.com>
2018-01-18 21:07:02 -0600

    added email and pass auth

--- fb-auth.js
+++ fb-auth.js
@@ -1,9 +1,70 @@
-const auth = firebase.auth();
 
-auth.siginInWithEmailAndPassword(email, pass);
 
-auth.createUserWithEmailAndPassword(email, pass);
 
-auth.onAuthStateChanged(firebaseUser => {
-    
+
+// auth.createUserWithEmailAndPassword(email, pass);
+
+// auth.onAuthStateChanged(firebaseUser => {
+
+// })
+
+( function(){
+
+var config = {
+    apiKey: "AIzaSyCj0YA_0hp76rEIQVl06sX0rGWMum6Ooxs",
+    authDomain: "fb-auth-practice.firebaseapp.com",
+    databaseURL: "https://fb-auth-practice.firebaseio.com",
+    projectId: "fb-auth-practice",
+    storageBucket: "fb-auth-practice.appspot.com",
+    messagingSenderId: "188662414494"
+};
+firebase.initializeApp(config);
+
+// Get Elements
+const txtEmail = document.getElementById("txtEmail");
+const txtPassword = document.getElementById("txtPassword");
+const btnLogin = document.getElementById("btnLogin");
+const btnSignUp = document.getElementById("btnSignUp");
+const btnLogout = document.getElementById("btnLogout");
+
+// Add login event
+btnLogin.addEventListener("click", e =>{
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+});
+
+// Add sign up event
+
+btnSignUp.addEventListener("click", e =>{
+    // TO:DO Check for real email
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
 })
+
+
+}());
+
+btnLogout.addEventListener("click", e =>{
+    firebase.auth().signOut();
+})
+
+// Add a realtime listener 
+firebase.auth().onAuthStateChanged(firebaseUser =>{
+    if (firebaseUser) {
+        console.log(firebaseUser);
+        btnLogout.classList.remove('hide');
+    } else { 
+        console.log('not logged in');
+        btnLogout.classList.add('hide');
+    }
+});
+        
+        
